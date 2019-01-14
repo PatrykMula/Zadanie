ba446f3f4f4df5bfb76abdd782a67d50879b4fbf
PatrykMula <patryk.mula@gmail.com>
2019-01-14 19:53:03 +0100

    Dodaj pliki projektów.

---
+++ Zadanie/Controllers/ZadanieController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadanie.Models;
+
+namespace Zadanie.Controllers
+{
+    public class ZadanieController : Controller
+    {
+        // GET: Zadanie
+        public ActionResult IndexKafelki()
+        {
+ //w przyszlosci te dane beda pobieranie z moq, albo z bazy danych
+            {
+                var taskList = new List<Dane>{
+                            new Dane() { id= 1,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 2,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 3,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 4,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 5,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 6,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 7,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            },
+                            new Dane() { id= 8,
+                                         temat = "temat1",
+                                         czynnosc = "czynnosc1",
+                                         opis = "jakis tam opis",
+
+                                         status = 1,
+                                         priorytet = 2,
+                                         procent_zakonczenia = 10,
+
+                                         data_rozpoczecia = DateTime.Now,
+                                         data_zakonczenia = DateTime.Now
+                            }
+
+
+
+                        };
+
+
+                return View(taskList);
+            }
+            //return View();
+        }
+    }
+}
---
+++ Zadanie/Models/Dane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zadanie.Models
+{
+    public class Dane
+    {
+        public decimal id { get; set; }
+        public string temat { get; set; }
+        public string czynnosc { get; set; }
+        public string opis { get; set; }
+
+        public decimal status { get; set; }
+        public decimal priorytet { get; set; }
+        public decimal procent_zakonczenia { get; set; }
+
+        public DateTime data_rozpoczecia { get; set;}
+        public DateTime data_zakonczenia { get; set; }
+    }
+}
